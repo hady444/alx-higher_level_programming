3e8848399c5eab740135d31ab74fca173875a8b4
hady444 <hadyhassan2312@gmail.com.com>
2023-10-31 16:57:28 +0000

    c8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str[0:-1]:
+    for c in str:
+        if c == ' ':
+            print(" ")
+            continue
         print("{}".format(chr(ord(c) - (ord('a') - ord('A')))), end='')
-    print("{}".format(chr(ord(str[-1]) - (ord('a') - ord('A')))))
