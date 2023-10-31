5a7df62b5a9dc6f86874035e2fb194a6f0351381
hady444 <hadyhassan2312@gmail.com.com>
2023-10-31 17:08:02 +0000

    c8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i, c in enumerate(str):
-        if ord(c) in (97, 113):
+        if ord(c) in (97, 123):
             print("{}".format(chr(ord(c) - (ord('a') - ord('A')))), end='')
         else:
             print("{}".format(c), end='')
