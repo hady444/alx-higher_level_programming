2ef212b326506c0b8193ce560d2e9cb271376378
hady444 <hadyhassan2312@gmail.com.com>
2023-10-31 16:36:17 +0000

    c6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(9):
     j = i + 1
     while (j < 10):
+        if i * 10 + j == 89:
+            print(89)
+            break
         print("{:02d}".format(i * 10 + j), end=', ')
         j += 1
