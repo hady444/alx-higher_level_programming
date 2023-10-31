c46b9644af0844ea3c8e378cb8e44a03bb2d1628
hady444 <hadyhassan2312@gmail.com.com>
2023-10-31 17:42:02 +0000

    c13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * insert_node - insert node to sorted list in approprate index
+ * @head: pointer to head
+ * @number: number to be set in node
+ * Return: address of inserted node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *prev;
+
+	if (!head)
+		return (NULL);
+	new->n = number;
+	if (number < *(head)->n)
+	{
+		new->next = *(head)->next;
+		*head = new;
+		return (new);
+	}
+	prev = *head;
+	while (prev->next)
+	{
+		if (number < (prev->next)->n)
+		{
+			new->next = prev->next;
+			prev->next = new;
+			return (new);
+		}
+		prev = prev->next
+	}
+	new->next = NULL;
+	prev->next = new;
+	return (new);
+}
