dcff2e3ab5afc7f78a5fe8469d16f35821251cf1
hady444 <hadyhassan2312@gmail.com.com>
2023-10-31 18:25:09 +0000

    c100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i - (ord('a') - ord('A'))), end='')
+    if i % 2 == 1:
+        i -= (ord('a') - ord('A'))
+    print(chr(i), end='')
