587303959d845959c2687521609ab87585bc7861
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 22:46:56 +0000

    c4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-import py_compile
-import dis
-if __name__ != "__main__":
-    exit()
-py_compile.compile('hidden_4.pyc')
 import hidden_4
 names = dir(hidden_4)
 filtered_names = sorted(name for name in names if not name.startswith("__"))
