d06c6aef046ccbac0ba1f9ebcd70ddb3ece8c4b0
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 22:47:52 +0000

    c4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
+if __name__ != "__main__":
+    exit()
 names = dir(hidden_4)
 filtered_names = sorted(name for name in names if not name.startswith("__"))
 for name in filtered_names:
