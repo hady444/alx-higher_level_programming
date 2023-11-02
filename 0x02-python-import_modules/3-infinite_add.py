c75988795e3b2e85cf890d1219f1e97712767b92
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 22:13:46 +0000

    c3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+import sys
 if __name__ != "__main__":
     exit()
 
-argc = len(sys.argv) - 1
-
-i = 0
-result = 0
+i = 1
+summ = 0
 for arg in sys.argv:
-    if i != 0:
-        result += int(arg)
-    else:
-        i += 1
-print("{:d}".format(result))
-#
-#import sys
-#if __name__ != "main":
-#    exit()
-
-#i = 1
-#summ = 0
-#for arg in sys.argv:
-#    if i != 1:
-#        summ += int(arg)
-#        continue
-#    i += 1
-#print("{:d}".format(summ))
+    if i != 1:
+        summ += int(arg)
+        continue
+    i += 1
+print(summ)
