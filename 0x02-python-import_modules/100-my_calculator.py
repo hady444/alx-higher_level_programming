e6857eca8b1e592e595f8da4eef4e968552bee40
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 23:03:24 +0000

    c100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ import sys
 from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
     exit()
-if len(sys.argv) == 1:
+if len(sys.argv) != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 if sys.argv[2] not in "+-*/":
