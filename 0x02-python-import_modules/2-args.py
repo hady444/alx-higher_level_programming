d125ae52ab2ba291567bb769c45700c47b336223
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 21:32:34 +0000

    c2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,12 @@ if __name__ != "__main__":
     exit()
 
 argc = len(sys.argv)
-print("{:d}: argumnts".format(argc - 1), end='')
+print("{:d}: argumnt".format(argc - 1), end='')
 if argc == 1:
-    print(".")
-else:
+    print("s.")
+elif argc == 2:
     print(":")
+else:
+    print("s:")
     for i in range(1, argc):
         print("{:d}: {}".format(i, sys.argv[i]))
