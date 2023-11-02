f505bd1479b6d3513d21029641adc4ab4b475b4c
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 21:53:40 +0000

    c2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ != "__main__":
 argc = len(sys.argv)
 arg_S = "{:d}: argument"
 if argc == 1:
-    arg_S += "s."
+    arg_S += 's.'
 elif argc == 2:
-    arg_S += ":"
+    arg_S += ':'
 else:
-    arg_S += "s:"
+    arg_S += 's:'
 print(arg_S.format(argc - 1))
 for i in range(1, argc):
-    print("{:d}: {}".format(i, sys.argv[i]))
+    print("{:d}: {:s}".format(i, sys.argv[i]))
