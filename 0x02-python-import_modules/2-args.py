67993faacdaaab5e607b102f962bc9621edcc97c
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 21:27:19 +0000

    c2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 import sys
 if __name__ != "__main__":
     exit()
+
 argc = len(sys.argv)
 if argc == 1:
-    print("1: argumnts.")
+    print("0: argumnts.")
 else:
     print("{:d}: argumnts:".format(argc - 1))
     for i in range(1, argc):
