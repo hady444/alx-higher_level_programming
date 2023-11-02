52db4abf92a49760f6f32e2cb467120dcbc1f28c
hady444 <hadyhassan2312@gmail.com.com>
2023-11-02 21:47:35 +0000

    c2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ != "__main__":
     exit()
 
-argStr = "{:d} argument"
-argc = len(sys.argv) - 1
-if argc == 0:
-    argStr += 's.'
-elif argc == 1:
-    argStr += ':'
+argc = len(sys.argv)
+arg_S = "{:d}: argument"
+if argc == 1:
+    arg_S += "s."
+elif argc == 2:
+    arg_S += ":"
 else:
-    argStr += 's:'
-print(argStr.format(argc))
-
-i = 0
-for arg in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, arg))
-    i += 1
+    arg_S += "s:"
+print(arg_S.format(argc - 1))
+for i in range(1, argc):
+    print("{:d}: {}".format(i, sys.argv[i]))
