73a6d36cf0b251f360ec87f4a597b08b8ccf75f7
hady444 <hadyhassan2312@gmail.com.com>
2023-11-04 10:00:37 +0000

    c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     my_string = list(my_string)
     for i, letter in enumerate(my_string):
-        if letter == 'c':
+        if letter in 'cC':
             my_string.pop(i)
     return (''.join(my_string))
