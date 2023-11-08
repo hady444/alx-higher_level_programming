ebf59514f4d7649e706649c52c190175dd786882
hady444 <hadyhassan2312@gmail.com.com>
2023-11-08 19:23:22 +0000

    c13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    numenator, denomenator = 0, 0
-    for i in my_list:
-        numenator += i[0] * i[1]
-        denomenator += i[1]
-    return numenator / denomenator
+    numenator, denomenator, avg = 0, 0, 0
+    if my_list:
+        for i in my_list:
+            numenator += i[0] * i[1]
+            denomenator += i[1]
+        avg = numenator / denomenator
+    return avg
