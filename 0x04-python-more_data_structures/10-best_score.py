e24ddc5ee1d13dca8a20b6ec099d3e552028ffba
hady444 <hadyhassan2312@gmail.com.com>
2023-11-08 16:45:07 +0000

    c10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,9 +4,3 @@ def best_score(a_dictionary):
         return None
     sorted_dict = {key: value for key, value in sorted(a_dictionary.items(), key=lambda item: item[1])}
     return list(sorted_dict.keys())[-1]
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
