700d7df2ef837efebf70384e6a1677aacaa3ae3d
hady444 <hadyhassan2312@gmail.com.com>
2023-11-08 19:06:30 +0000

    c12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     number = 0
-    Romans = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M':1000}
+    Romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) is str and roman_string:
         for i, N in enumerate(roman_string):
             if N in Romans.keys():
