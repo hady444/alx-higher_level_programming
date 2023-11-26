575a68ee7c59170ec6ef9c84c08736381501282b
hady444 <hadyhassan2312@gmail.com.com>
2023-11-26 18:08:57 +0000

    c

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square4"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        """setter"""
+        self.__size = size
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for _ in range(self.__size):
+            for _ in range(self.__size):
+                print("#",end='')
+            print()
