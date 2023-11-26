a36b850a8961129d5e15fd5cd2efafb425230224
hady444 <hadyhassan2312@gmail.com.com>
2023-11-26 18:03:43 +0000

    c

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square4"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, sizei=0):
+        """setter"""
+        self.__size = size
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size**2
