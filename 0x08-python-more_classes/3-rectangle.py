70d7941c5de57a0eec11c3c6e1cf2fcca4e217db
hady444 <hadyhassan2312@gmail.com.com>
2023-11-27 19:05:45 +0000

    c

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Rectangle:
+    """
+    Rectangle 3
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        if self.__width == 0 or self.height == 0:
+            print()
+            return
+        for _ in range(self.__heigth):
+            for _ in range(self.__width):
+                print('#', end='')
+            print()
