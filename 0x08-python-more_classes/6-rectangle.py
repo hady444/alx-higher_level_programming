7655e0e3ff1fd5cd2b78c697e117f1d406f6dcca
hady444 <hadyhassan2312@gmail.com.com>
2023-11-27 19:41:44 +0000

    c

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-        Rectangle.number_of_instances += Rectangle.number_of_instances + 1
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
