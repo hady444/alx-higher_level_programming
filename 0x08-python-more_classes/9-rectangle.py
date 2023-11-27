94f987e7cadff90cbe8f09feefbb455050c29aeb
hady444 <hadyhassan2312@gmail.com.com>
2023-11-27 20:41:06 +0000

    c

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,7 +22,8 @@ class Rectangle:
         return "\n".join(x)
 
     def __repr__(self):
-        return f'{type(self).__name__}({self.__width}, {self.__height})'
+        return "{:s}({:d}, {:d})".format((type(self).__name__),
+                                         self.__width, self.__height)
 
     def __del__(self):
         print("Bye rectangle...")
