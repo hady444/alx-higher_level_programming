11ee903110f0f9ae1deea6c0203736d7341cefc9
hady444 <hadyhassan2312@gmail.com.com>
2023-12-01 21:36:09 +0000

    c

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,10 @@ Module
 
 
 import unittest
+
+
 def max_integer(list=[]):
-    """Get max number 
+    """Get max number
 
     Args:
         list (list):list contain numbers. Defaults to [].
@@ -15,19 +17,29 @@ def max_integer(list=[]):
     """
     return max(list)
 
+
 class TestMaxInteger(unittest.TestCase):
+    """
+    class Test my function
+    """
     def Test_Max_End(self):
         self.assertEqual(max_integer([10, 100, 300, 5000]), 5000)
+
     def Test_Max_Begin(self):
         self.assertEqual(max_integer([5000, 10, 100, 300]), 5000)
+
     def Test_Max_Middle(self):
         self.assertEqual(max_integer([10, 20, 15000, 300, 5000]), 15000)
+
     def Test_one_Negative(self):
-        self.assertEqual(max_integer([10,100,300,-5000]), 300)
+        self.assertEqual(max_integer([10, 100, 300, -5000]), 300)
+
     def Test_All_Negative(self):
-        self.assertEqual(max_integer([-10,-100,-300,-5000]), -10)
+        self.assertEqual(max_integer([-10, -100, -300, -5000]), -10)
+
     def Test_One_Element(self):
         self.assertEqual(max_integer([10]), 10)
+
     def Test_Is_empty(self):
         with self.assertRaises(ValueError):
             max_integer([])
