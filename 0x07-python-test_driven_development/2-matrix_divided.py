ac118b07083a4e43c7aa3ba90a89f9151b74815a
hady444 <hadyhassan2312@gmail.com.com>
2023-12-01 17:19:15 +0000

    t2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-def find_dimensions(lst):
-    if not isinstance(lst, list):
-        return 0
-    if not lst:
-        return 1
-    inner_dimension = find_dimensions(lst[0])
-    return inner_dimension + 1
+"""
+Module
+"""
+
 
 def matrix_divided(matrix, div):
     result = []
-    if find_dimensions(matrix) != 2:
+    if not not all(isinstance(row, list) for row in matrix)
+    or not all(isinstance(val, (int, float)) for row in matrix for val in row)
+    or len(matrix) == 0
+    or any(len(row) == 0 for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) " +\
                 "of integers/floats")
     l = len(matrix[0])
@@ -22,9 +22,6 @@ def matrix_divided(matrix, div):
         if l != len(ls):
             raise TypeError("Each row of the matrix must have the same size")
         for x in ls:
-            if not isinstance(x, int) and not isinstance(x, float):
-                raise TypeError("matrix must be a matrix (list of lists) " +\
-                        "of integers/floats")
             row.append(format(x/ div, '.2f'))
         result.append(row)
         row = []
