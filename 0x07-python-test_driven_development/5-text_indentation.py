c40d41a0ee58c801ceb2c2023eb13222dfbffaf6
hady444 <hadyhassan2312@gmail.com.com>
2023-12-01 19:57:31 +0000

    c

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,7 +16,6 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for delim in ".?:":
-        #print(delim, text.split(delim))
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
 
