3faac367fe07924794c19be20c525bc980b00710
hady444 <hadyhassan2312@gmail.com.com>
2023-12-01 21:32:28 +0000

    c

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
+import unittest
+def max_integer(list=[]):
+    """Get max number 
+
+    Args:
+        list (list):list contain numbers. Defaults to [].
+    Return:
+        max number
+    """
+    return max(list)
+
+class TestMaxInteger(unittest.TestCase):
+    def Test_Max_End(self):
+        self.assertEqual(max_integer([10, 100, 300, 5000]), 5000)
+    def Test_Max_Begin(self):
+        self.assertEqual(max_integer([5000, 10, 100, 300]), 5000)
+    def Test_Max_Middle(self):
+        self.assertEqual(max_integer([10, 20, 15000, 300, 5000]), 15000)
+    def Test_one_Negative(self):
+        self.assertEqual(max_integer([10,100,300,-5000]), 300)
+    def Test_All_Negative(self):
+        self.assertEqual(max_integer([-10,-100,-300,-5000]), -10)
+    def Test_One_Element(self):
+        self.assertEqual(max_integer([10]), 10)
+    def Test_Is_empty(self):
+        with self.assertRaises(ValueError):
+            max_integer([])
