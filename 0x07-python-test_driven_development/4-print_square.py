ecee9b77ad1d6291c203550825a27f4085606936
hady444 <hadyhassan2312@gmail.com.com>
2023-12-01 19:07:12 +0000

    c

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def print_square(size):
+    """function prits a square of #
+
+    Args:
+        size (int): the size length of the square
+    Raises:
+        TypeError: if size is not int or
+                    (less than zero and float)
+        ValueError: if less than zero only
+    """
+    if (isinstance(size, float) and size < 0) or not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    my_result = []
+    for _ in range(size):
+        my_result.append(["#" * size])
+    flattened_result = [str(item) for sublist in my_result for item in sublist]
+    print('\n'.join(flattened_result))
+
+print_square()
