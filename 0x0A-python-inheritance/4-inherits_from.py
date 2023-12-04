7b418cf1f065bf04c0fc3ed01e78d3ee7043c07b
hady444 <hadyhassan2312@gmail.com.com>
2023-12-04 17:22:10 +0000

    c4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+is object from this class
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if object from provided class
+
+    Args:
+        obj: object passed
+        a_class: class
+    Return:
+        True if in
+        False if out
+    """
+    return (isinstance(obj, a_class))
