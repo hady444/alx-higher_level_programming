98b56c2ef748c58b168c556eecec91dc583210ca
hady444 <hadyhassan2312@gmail.com.com>
2023-12-05 21:32:14 +0000

    c6

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module"""
+from os import path
+import json
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+pairs = []
+if os.path.exists("add_item.json")
+prev_content = load_from_json_file("add_item.json")
+for index in argv[1:]:
+    pairs.append(index)
+save_to_json_file(pairs, "add_item.json")
