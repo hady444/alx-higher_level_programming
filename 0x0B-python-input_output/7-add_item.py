1f1fbb752b2f192fd3c6d6327b0043519ff35835
hady444 <hadyhassan2312@gmail.com.com>
2023-12-05 21:36:06 +0000

    c7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 pairs = []
-if os.path.exists("add_item.json")
-prev_content = load_from_json_file("add_item.json")
+if os.path.exists("add_item.json"):
+    prev_content = load_from_json_file("add_item.json")
 for index in argv[1:]:
     pairs.append(index)
 save_to_json_file(pairs, "add_item.json")
