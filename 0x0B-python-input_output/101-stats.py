60a37ef303f7b97136e161b1fa257636ad98921b
hady444 <hadyhassan2312@gmail.com.com>
2023-12-06 19:55:44 +0000

    c101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -28,7 +28,7 @@ try:
         if len(line_s) >= 2:
             if line_s[-2] in status_code:
                 status_code[line_s[-2]] += 1
-            file_size += int(line_s[-1].split("\n")[0])
+            file_size += int(line_s[-1])
             if i % 10 == 0 and i != 0:
                 print_stats(file_size, status_code)
         i += 1
