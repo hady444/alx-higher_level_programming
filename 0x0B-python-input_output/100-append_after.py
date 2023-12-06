8fc2bea597d5cab338580ebf8c5d7e6bc732a571
hady444 <hadyhassan2312@gmail.com.com>
2023-12-06 17:42:50 +0000

    c100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,13 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """append new string after each line search string is in.
+
+    Args:
+        filename (str): path to file. Defaults to "".
+        search_string (str): string to search for. Defaults to "".
+        new_string (str): string to write. Defaults to "".
+    """
     with open(filename, 'r+', encoding='UTF-8') as f:
         i = -1
         for line in f.readlines():
