c76caa93126aa42ea231dcb8eaff1a905ec08434
hady444 <hadyhassan2312@gmail.com.com>
2023-12-06 17:10:02 +0000

    c11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Student:
+    """
+    Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs is None):
+            return self.__dict__
+        my_dictionary = {}
+        for k, val in self.__dict__.items():
+            if k in attrs:
+                my_dictionary[k] = val
+        return (my_dictionary)
+
+    def reload_from_json(self, json):
+        for k, val in json.items():
+            setattr(self, k, val)
