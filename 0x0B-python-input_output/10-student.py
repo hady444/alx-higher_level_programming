f3575d3f5a461d7f26dbe5df39d7f126ca7c226e
hady444 <hadyhassan2312@gmail.com.com>
2023-12-06 17:00:45 +0000

    c10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs==None:
+        if (attrs is None):
             return self.__dict__
         my_dictionary = {}
         for k, val in self.__dict__.items():
