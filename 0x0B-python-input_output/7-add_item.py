78793e9242367473b59f187b3372890ccee846cf
hady444 <hadyhassan2312@gmail.com.com>
2023-12-06 16:12:51 +0000

    c7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Module"""
 
-
+import os.path
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
-js_list = [load_from_json_file("arg_item.json")]
+try:
+    js_list = load_from_json_file("add_item.json")
+except:
+    js_list = []
 for arg in arguments:
     js_list.append(arg)
-js_list = save_to_json_file(js_list, "arg_item.json)
+js_list = save_to_json_file(js_list, "add_item.json")
