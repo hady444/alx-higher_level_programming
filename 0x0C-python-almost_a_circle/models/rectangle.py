625ce7a1181f9bc176145c8c4d450eed5d2268b4
hady444 <hadyhassan2312@gmail.com.com>
2023-12-11 21:41:23 +0000

    c

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,12 +88,23 @@ class Rectangle(Base):
                 print("#", end='')
             print("")
 
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
     def update(self, *args, **kwargs):
-        if args == ():
-            for kw, val in kwargs.items():
-                setattr(self, kw, val)
-
-        attribute_names = ['id', 'width', 'height', 'x', 'y']
-
-        for name, value in zip(attribute_names, args):
-            setattr(self, name, value)
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        # print(args, kwargs)
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
