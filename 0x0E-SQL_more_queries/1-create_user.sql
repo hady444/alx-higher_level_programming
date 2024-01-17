df8a41074f588dfe12a25873b1a08aa04b432a23
hady444 <hadyhassan2312@gmail.com.com>
2024-01-17 16:38:07 +0000

    Task 1 v1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES * . * ON 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
