221121cdfa62f6b0cc584268238047d13d22201b
hady444 <hadyhassan2312@gmail.com.com>
2024-01-17 01:56:02 +0000

    Task 100 v1

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,6 @@
--- Filename: 100-move_to_utf8.sql
+-- Change character SET and COLLATE
 
--- Convert the database to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF-8
-USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the 'name' field to UTF-8
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
