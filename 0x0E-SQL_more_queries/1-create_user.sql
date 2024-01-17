c3a12c3aea046e280067048f072e4b793d5d916d
hady444 <hadyhassan2312@gmail.com.com>
2024-01-17 16:42:25 +0000

    Task 1 v4

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- script that creates the MySQL server user user_0d_1.
+-- Filename: 1-create_user.sql
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
