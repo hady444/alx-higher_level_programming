1336eeb18f4f3f198ff34e07b707cd897c5a0584
Hady Hassan <99578132+hady444@users.noreply.github.com>
2024-01-19 22:16:37 +0200

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,6 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
 -- Convert the table to utf8mb4 with the specified collation
 ALTER TABLE first_table
     CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
