c5a3a3c34e56c960e7cf224415b8d9b2d7e82a52
Hady Hassan <99578132+hady444@users.noreply.github.com>
2024-01-19 22:23:45 +0200

    Update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,10 @@
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Filename: 100-move_to_utf8.sql
 
--- Convert the table to utf8mb4 with the specified collation
-ALTER TABLE first_table
-    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the database to UTF-8
+ALTER DATABASE IF EXISTS hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the name field to utf8mb4 with the specified collation
-ALTER TABLE first_table
-    MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the table to UTF-8
+ALTER TABLE IF EXISTS first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the 'name' field to UTF-8
+ALTER TABLE IF EXISTS first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
