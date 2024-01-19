c53e756d01a059df1e968d020766de6bb3a8febb
Hady Hassan <99578132+hady444@users.noreply.github.com>
2024-01-19 22:14:51 +0200

    Update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
--- converts hbtn_0c_0 database to utf8
+-- Convert the table to utf8mb4 with the specified collation
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the name field to utf8mb4 with the specified collation
+ALTER TABLE first_table
+    MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
