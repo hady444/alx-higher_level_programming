36ee6b1a37255ea6dc5eb23196f06f4e68fd1723
hady444 <hadyhassan2312@gmail.com.com>
2024-02-12 19:39:32 +0000

    javascript 12

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const { argv } = require('process');
+let n1 = 0;
+let n2 = 0;
+let tmp = 0;
+if (argv.length < 3) {
+  console.log(0);
+} else {
+  if (parseInt(argv[2]) > parseInt(argv[3])) {
+    n1 = parseInt(argv[2]);
+    n2 = parseInt(argv[3]);
+  } else {
+    n1 = parseInt(argv[3]);
+    n2 = parseInt(argv[2]);
+  }
+  for (let i = 4; i < argv.length; i++) {
+    if (n1 < parseInt(argv[i])) {
+      tmp = n1;
+      n1 = parseInt(argv[i]);
+      n2 = tmp;
+    } else if (n2 < parseInt(argv[i])) {
+      n2 = parseInt(argv[i]);
+    }
+  }
+  console.log(n2);
+}
