9bd80553ef3c2bd9c845808c3879e249e4d28d36
hady444 <hadyhassan2312@gmail.com.com>
2024-02-12 18:55:01 +0000

    javascript 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,5 +5,5 @@ function factorial (n) {
   } else {
     return n * factorial(n - 1);
   }
-};
-console.log(parseInt(factorial(process.argv[2])));
+}
+console.log(factorial(Number(process.argv[2])));
