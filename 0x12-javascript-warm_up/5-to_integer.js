ca77a261151d267f6919c807feb6926009b7c537
hady444 <hadyhassan2312@gmail.com.com>
2024-02-12 14:11:59 +0000

    javascript 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
-let num = parseInt(argv[2]);
+const num = parseInt(argv[2]);
 if (Number.isInteger(num)) {
   console.log(`My number: ${num}`);
 } else {
