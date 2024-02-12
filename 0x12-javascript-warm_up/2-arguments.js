d4ada27787d15862c220e695db8a7f817c1ff047
hady444 <hadyhassan2312@gmail.com.com>
2024-02-12 13:46:03 +0000

    javascript 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length > 2)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('No argument');
+if (argv.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
