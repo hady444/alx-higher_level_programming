d5c0fe782091329acccb4ba1c625abb8896036b6
hady444 <hadyhassan2312@gmail.com.com>
2024-02-12 18:47:40 +0000

    javascript 11

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (num === 0 | num === 1 | !(num)) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+console.log(parseInt(factorial(process.argv[2])));
