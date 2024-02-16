6a9e289db60a5b6d25953f44fc6d64dc457cceed
hady444 <hadyhassan2312@gmail.com.com>
2024-02-16 18:11:10 +0000

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             port=3306,
             db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name Like('N%')")
+    cur.execute("SELECT * FROM states WHERE name Like('N%') ORDER BY id ASC")
     data = cur.fetchall()
     for row in data:
         print(row)
