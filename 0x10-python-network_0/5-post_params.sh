02b1f834e997fc8da4ef02c99762a48af0ae78aa
hady444 <hady444@github.com>
2024-03-01 01:42:57 +0200

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request URL, and displays the body
-curl -sX Post -d "'email': 'test@gmail.com', 'subject': 'I will always be here for PLD'" $1
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
