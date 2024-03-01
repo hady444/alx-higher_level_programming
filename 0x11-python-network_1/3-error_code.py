8aeb3838099487168ae2f8cf919e7a71d4c7601f
hady444 <hady444@github.com>
2024-03-01 15:11:42 +0200

    tasl 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    takes in a URL, sends a request to the URL and
+    displays the body of the response
+"""
+
+
+import urllib.request
+import urllib.error
+import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen() as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
