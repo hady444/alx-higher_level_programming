d39f159dd20e2dd24b4e62c7c63aa62b7e4ab102
hady444 <hady444@github.com>
2024-03-01 22:45:53 +0200

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,5 @@ if __name__ == '__main__':
             print("No result")
         else:
             print("[{}] {}".format(r.get("id"), r.get("name")))
-    except ValueError as e:
+    except ValueError:
         print("Not a valid JSON")
