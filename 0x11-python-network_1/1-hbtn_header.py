a3f3937951d5755f55d066180c29906a5e383541
hady444 <hady444@github.com>
2024-03-01 14:43:40 +0200

    tasl 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,6 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
     print(response.headers.get('X-Request-Id'))
