e3cb0abc8f76b80bf9e1196a0525cd5b2c68391a
hady444 <hady444@github.com>
2024-03-01 15:13:19 +0200

    tasl 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen() as response:
+        with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
